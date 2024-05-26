ed3e262ac5d46f3b21493680194277b239f60a08
generyand <generyan.dep@gmail.com>
2024-05-26 09:07:09 +0800

    Changed the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>Chatteru</title>
+      </head>
       <body className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}>
         <div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
           <Header />
