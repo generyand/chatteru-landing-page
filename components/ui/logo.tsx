439194413529d0b709601759cd613a27c8c33629
generyand <generyan.dep@gmail.com>
2024-05-26 09:53:03 +0800

    changed logo

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,9 +1,10 @@
-import Link from 'next/link'
+import Link from "next/link";
+import logo from "../../public/images/logo-icon.png";
 
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
-      <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+      {/* <svg className="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
         <defs>
           <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="footer-logo">
             <stop stopColor="#4FD1C5" offset="0%" />
@@ -12,7 +13,10 @@ export default function Logo() {
           </radialGradient>
         </defs>
         <rect width="32" height="32" rx="16" fill="url(#footer-logo)" fillRule="nonzero" />
-      </svg>
+      </svg> */}
+      <div className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-400">
+        Chatteru
+      </div>
     </Link>
-  )
+  );
 }
