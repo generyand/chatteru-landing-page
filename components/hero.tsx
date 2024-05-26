55514d641ae724d9ba4c122ae719fefcadb8844e
generyand <generyan.dep@gmail.com>
2024-05-27 07:50:52 +0800

    changed download link to google drive

--- components/hero.tsx
+++ components/hero.tsx
@@ -68,7 +68,7 @@ export default function Hero() {
                   {/* DOWNLOAD LINK USING GOOGLE DRIVE */}
                   <a
                     className="w-full mb-4 text-white bg-blue-600 btn hover:bg-blue-700 sm:w-auto sm:mb-0"
-                    href="./../public/application/Chatteru.apk"
+                    href="https://drive.google.com/file/d/1cOIXWD2q4COulpCRxzzma9aW_Wxe9LzG/view?usp=sharing"
                     target="_blank"
                     download
                   >
