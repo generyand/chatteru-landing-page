a10bb9d8e184966adfbc8d45069847870cd194a9
generyand <generyan.dep@gmail.com>
2024-05-27 07:09:11 +0800

    minor changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -68,7 +68,7 @@ export default function Hero() {
                   {/* DOWNLOAD LINK USING GOOGLE DRIVE */}
                   <a
                     className="w-full mb-4 text-white bg-blue-600 btn hover:bg-blue-700 sm:w-auto sm:mb-0"
-                    href="https://drive.google.com/file/d/1-lDfbeuKyB1p7A2ysLqEHjYoBfXAmlEA/view?usp=sharing"
+                    href="../../application/Chatteru.apk"
                     target="_blank"
                   >
                     Download
